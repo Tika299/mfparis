76c8de2140548bd2d3c67327fbb526cc735466ff
Tika299 <vukofa9120@gmail.com>
2026-06-08 11:51:28 +0700

    make checkout page dynamic

--- src/app/(frontend)/checkout/success/page.tsx
+++ src/app/(frontend)/checkout/success/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+export const fetchCache = 'force-no-store'
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 import { CheckCircle } from 'lucide-react'
