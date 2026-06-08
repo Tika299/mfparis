14a566ecd7af74e82f2d5312569a15a86be1ce09
Tika299 <vukofa9120@gmail.com>
2026-06-08 12:17:28 +0700

    make frontend routes dynamic

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,3 +1,6 @@
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+export const fetchCache = 'force-no-store'
 import { Header } from '@/components/Header'
 import { Footer } from '@/components/Footer'
 import '../globals.css'
