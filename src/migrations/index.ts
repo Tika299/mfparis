fcc39952decbf6723d3162e1a4e10fb720a5369f
Tika299 <vukofa9120@gmail.com>
2026-06-17 17:01:48 +0700

    Refactor code structure for improved readability and maintainability

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,27 +1,9 @@
-import * as migration_20260608_121434 from './20260608_121434';
-import * as migration_20260615_091211_redirects_manager from './20260615_091211_redirects_manager';
-import * as migration_20260616_075715_add_carts_and_product_attributes from './20260616_075715_add_carts_and_product_attributes';
-import * as migration_20260617_094038 from './20260617_094038';
+import * as migration_20260617_095946 from './20260617_095946';
 
 export const migrations = [
   {
-    up: migration_20260608_121434.up,
-    down: migration_20260608_121434.down,
-    name: '20260608_121434',
-  },
-  {
-    up: migration_20260615_091211_redirects_manager.up,
-    down: migration_20260615_091211_redirects_manager.down,
-    name: '20260615_091211_redirects_manager',
-  },
-  {
-    up: migration_20260616_075715_add_carts_and_product_attributes.up,
-    down: migration_20260616_075715_add_carts_and_product_attributes.down,
-    name: '20260616_075715_add_carts_and_product_attributes',
-  },
-  {
-    up: migration_20260617_094038.up,
-    down: migration_20260617_094038.down,
-    name: '20260617_094038'
+    up: migration_20260617_095946.up,
+    down: migration_20260617_095946.down,
+    name: '20260617_095946'
   },
 ];
